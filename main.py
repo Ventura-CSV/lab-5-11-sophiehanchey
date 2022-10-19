6c3b2945f2c9211883328f409810321ac26749e3
Kyuwoong Lee <kwooler@gmail.com>
2022-10-19 00:56:27 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random 
+
+
+def foldandswap(numbers):
+	#################
+	# Make your code 
+	#################
+
+
+numbers = [2,3,0,5,4,1,6,9,8,7]
+print (numbers)
+foldandswap(numbers)
+print (numbers)
+
+
+numbers = [ random.randint(0,10) for i in range(10)]
+print (numbers)
+foldandswap(numbers)
+print (numbers)
+
+
