fa82f24912a3ac0b6bbaf3f59e7882dc51636c5a
Kyu Lee <kwooler@gmail.com>
2022-10-20 00:45:26 -0700

    Lab 11

--- main_test.py
+++ main_test.py
@@ -1,21 +1,39 @@
-
+import random
 import main
 
 
 def test_main():
-    numbers = [5, 4, 3, 2, 1]
 
-    others = numbers
-    for i in range(len(numbers)-1):
-        findmin(others)
-        firstval, *others = others
-        numbers[i] = firstval
+    number1 = [0, 2, 3]
+    number2 = [1, 4, 5, 6, 9]
+    print('Test data')
+    print(number1)
+    print(number2)
+    retlist = main.merge(number1, number2)
+    print('After merge', retlist)
+    assert retlist[0] == 0
+    assert retlist[1] == 1
+    assert retlist[2] == 2
+    assert retlist[3] == 3
+    assert retlist[4] == 4
+    assert retlist[5] == 5
+    assert retlist[6] == 6
+    assert retlist[7] == 9
+    # #########################################
 
-    print(numbers)
-    assert numbers[0] == 1
-    assert numbers[1] == 2
-    assert numbers[2] == 3
-    assert numbers[3] == 4
-    assert numbers[4] == 5
+    n1 = [random.randint(0, 20) for i in range(5)]
+    n2 = [random.randint(0, 20) for i in range(3)]
+    n1.sort()
+    n2.sort()
+    tlist = n1 + n2
+    tlist.sort()
+    print('Test data')
+    print(n1)
+    print(n2)
+    retlist = main.merge(n1, n2)
+    print('After merge', retlist)
+    print(tlist)
+    print(retlist)
+    assert tlist == retlist
 # assert v1 == -10, "Min value does not match"
 # assert v2 == 5, "Max value does not match"
