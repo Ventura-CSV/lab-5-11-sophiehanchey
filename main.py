5e70d8f155c6c06a6187b1f58a1e80d919062ad3
Kyu Lee <kwooler@gmail.com>
2023-03-27 01:25:54 -0700

    L511

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-import random 
+import random
 
 
 def merge(number1, number2):
-	#################
-	# Make your code 
-	#################
-	return mlist
+    #################
+    # Make your code
+    #################
+    mlist = []
+
+    #################
+    # Do not delete return statement
+    # You should return 'mlst" as a merged result
+    return mlist
 
 
-number1 = [0, 2, 3]
-number2 = [1, 4, 5, 6, 9]
-retlist = merge(number1, number2)
-print (retlist)
-# #########################################
+def main():
+    number1 = [0, 2, 3]
+    number2 = [1, 4, 5, 6, 9]
+    retlist = merge(number1, number2)
+    print(retlist)
+    # #########################################
 
-n1 = [ random.randint(0,20) for i in range(5)]
-n2 = [ random.randint(0,20) for i in range(3)]
-n1.sort()
-n2.sort()
-print (n1)
-print (n2)
-retlist = merge(n1, n2)
-print (retlist)
+    n1 = [random.randint(0, 20) for i in range(5)]
+    n2 = [random.randint(0, 20) for i in range(3)]
+    n1.sort()
+    n2.sort()
+    print(n1)
+    print(n2)
+    retlist = merge(n1, n2)
+    print(retlist)
 
+
+if __name__ == '__main__':
+    main()
