99de6ab5d7ebadfd4244aca848e4db8d453cad47
Kyu Lee <kwooler@gmail.com>
2023-10-11 22:25:24 -0700

    L511

--- main_test.py
+++ main_test.py
@@ -38,38 +38,36 @@ def test_main_2():
     print(tlist)
     print(retlist)
     assert tlist == retlist
-# assert v1 == -10, "Min value does not match"
-# assert v2 == 5, "Max value does not match"
 
 
-# def check_file_for_sort_function(file_path):
-#     try:
-#         with open(file_path, 'r') as file:
-#             content = file.read()
-#             # Regular expression pattern to match 'sort('
-#             pattern = r'\bsort\s*\('
-#             matches = re.findall(pattern, content)
-#             for match in matches:
-#                 if not is_within_comment(content, match):
-#                     return True
-#             return False
-#     except FileNotFoundError:
-#         print(f"File '{file_path}' not found.")
-#         return False
+def check_file_for_sort_function(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            # Regular expression pattern to match 'sort('
+            pattern = r'\bsort\s*\('
+            matches = re.findall(pattern, content)
+            for match in matches:
+                if not is_within_comment(content, match):
+                    return True
+            return False
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return False
 
 
-# def is_within_comment(content, match):
-#     lines = content.split("\n")
-#     for line in lines:
-#         comment_start = line.find("#")
-#         if comment_start != -1 and comment_start < line.find(match):
-#             return True
-#     return False
+def is_within_comment(content, match):
+    lines = content.split("\n")
+    for line in lines:
+        comment_start = line.find("#")
+        if comment_start != -1 and comment_start < line.find(match):
+            return True
+    return False
 
 
-# def test_sort():
-#     flag = check_file_for_sort_function('main.py')
-#     assert flag == False, 'Do not use sort functions '
+def test_sort():
+    flag = check_file_for_sort_function('main.py')
+    assert flag == False, 'Do not use sort functions '
 
 # def test_sort():
 #     with open('main.py') as f:
